797357ace533755420c077784a71c42a39177a63
Youssef Waleed <youssef.waleed22470@ci.menofia.edu.eg>
2026-07-25 05:06:23 +0300

    Move FileLogger out of Logs

---
+++ Core/FileLogger.cs
@@ -0,0 +1,33 @@
+﻿namespace JOEX_DB_Engine.Engine;
+
+public class FileLogger
+{
+    private readonly string _logPath = "Logs/engine.log";
+
+    public FileLogger()
+    {
+        Directory.CreateDirectory("Logs");
+
+        if (!File.Exists(_logPath))
+            File.Create(_logPath).Dispose();
+    }
+
+    public void Log(string level, string message)
+    {
+        var line =
+            $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{level}] {message}";
+
+        File.AppendAllLines(_logPath, new[] { line });
+    }
+
+    public IEnumerable<string> GetLogs(int take = 100)
+    {
+        if (!File.Exists(_logPath))
+            return Enumerable.Empty<string>();
+
+        return File.ReadLines(_logPath)
+            .Reverse()
+            .Take(take)
+            .Reverse();
+    }
+}
